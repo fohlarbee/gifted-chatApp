85a78993230244c277eed4823365ef87667c1c5d
Samuel Olanrewaju <sammyola246@gmail.com>
2023-04-10 20:23:13 +0100

    fimal commit

--- firebase/firebaseconfig.js
+++ firebase/firebaseconfig.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore'
+import { firebaseConfig } from "../secrets/firebaseconfig.key";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC0npvRwLWwG-TcMHQVekfMQutCzDNXUpA",
-  authDomain: "giftedchatapp-fde9c.firebaseapp.com",
-  projectId: "giftedchatapp-fde9c",
-  storageBucket: "giftedchatapp-fde9c.appspot.com",
-  messagingSenderId: "909302892900",
-  appId: "1:909302892900:web:8a8601ee18fdd15a739271"
-};
 
 // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
---
+++ secrets/firebaseconfig.key.js
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyC0npvRwLWwG-TcMHQVekfMQutCzDNXUpA",
+    authDomain: "giftedchatapp-fde9c.firebaseapp.com",
+    projectId: "giftedchatapp-fde9c",
+    storageBucket: "giftedchatapp-fde9c.appspot.com",
+    messagingSenderId: "909302892900",
+    appId: "1:909302892900:web:8a8601ee18fdd15a739271"
+  };
